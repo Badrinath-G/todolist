9953c0d4db2c371f36a588809afedec68c84bd9d
Badrinath Gonnabattula <badrinathgonnabattula@gmail.com>
2020-07-14 17:15:11 -0700

    Updates

---
+++ precache-manifest.8d881e45412cb37863527ac34bbb96c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7bfe543d1f1ee9fb6b399797206d25",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b6541a58dc423507120d",
+    "url": "/todolist/static/css/main.a14c5d6b.chunk.css"
+  },
+  {
+    "revision": "650d8cad3f9d4fcd28bb",
+    "url": "/todolist/static/js/2.cabb37e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.cabb37e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6541a58dc423507120d",
+    "url": "/todolist/static/js/main.2189e658.chunk.js"
+  },
+  {
+    "revision": "5a41f0122cf00b6bc18f",
+    "url": "/todolist/static/js/runtime-main.8b55999f.js"
+  }
+]);
